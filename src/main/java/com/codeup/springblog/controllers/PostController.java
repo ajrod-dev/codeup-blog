c5ea6250137172b74b9d52aa15830b8481f8ab57
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-08-03 14:14:15 -0500

    roll-dice exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,19 +1,29 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class PostController {
 
     @GetMapping("/posts")
-    public String returnPosts(){
+    public String returnPosts(Model model){
+        List<Post> allPosts = new ArrayList<Post>();
+        allPosts.add(new Post("First Post", "Hello World"));
+        allPosts.add(new Post("Second Post", "Learning Spring"));
+        allPosts.add(new Post("Third Post", "And ThymeLeaf."));
+        model.addAttribute("posts", allPosts);
         return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String returnIndividualPosts(@PathVariable int id){
-        return "/posts/view";
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
